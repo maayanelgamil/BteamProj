e8ffa05c7c7410d71f5daff1a36697fafee107ef
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-03-30 21:32:49 +0300

    Add ranker

---
+++ bteam/bteam/Model/Ranker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bteam.Model
+{
+    public static class Ranker
+    {
+
+        public static double rank(double tags, double distFromLegth, double communTags)
+        {
+            double result = (tags + distFromLegth + communTags) / 3;
+            return result;
+        }
+    }
+}
