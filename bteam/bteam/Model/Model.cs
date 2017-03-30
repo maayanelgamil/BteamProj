d0514f54f6e1728b38ba1fc32af89f5d3b2692c8
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-03-30 18:09:20 +0300

    Add read text from file

--- bteam/bteam/Model/Model.cs
+++ bteam/bteam/Model/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace bteam.Model
 {
     class Model
     {
+        public Model()
+        {
+
+        }
+
+        public string getNumOfWords(string path)
+        {
+            string result;
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            return result;
+        }
     }
 }
