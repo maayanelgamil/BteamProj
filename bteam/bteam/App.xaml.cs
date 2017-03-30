f72bb7c9b1a54ba8e771a84079ce1d2d1316ffab
ben <ben@BEN-THINKPAD>
2017-03-30 20:14:52 +0300

    with tag function

--- bteam/bteam/App.xaml.cs
+++ bteam/bteam/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using bteam.Model;
 
 namespace bteam
 {
@@ -13,5 +14,8 @@ namespace bteam
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+        }
     }
 }
--- bteam/bteam/Model/html.cs
+++ bteam/bteam/Model/html.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,21 @@ using System.Threading.Tasks;
 
 namespace bteam.Model
 {
-    static class html
+    public static class html
     {
-        public static Dictionary<string,int> getTagsFromFile()
+        public static Dictionary<string,int> getTagsFromFile(string filePath)
         {
             Dictionary<string, int> res = new Dictionary<string, int>();
-
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.Load(filePath);
+            List<HtmlNode> allNodes = new List<HtmlNode>(htmlDoc.DocumentNode.Descendants());
+            foreach(HtmlNode node in allNodes)
+            {
+                if (res.ContainsKey(node.Name))
+                    res[node.Name]++;
+                else
+                    res.Add(node.Name, 1);
+            }
             return res;
         }
     }
