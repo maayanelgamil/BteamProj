fb7b880b5c7bee223441aad420e0b860fc419b14
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-03-31 09:54:07 +0300

    com

--- bteam/bteam/ViewModel.cs
+++ bteam/bteam/ViewModel.cs
@@ -65,49 +65,49 @@ namespace bteam
             get { return m_model.User8; }
         }
 
-        public double Avg
+        public string Avg
         {
-            get { return m_model.Avg; }
+            get { return "" + m_model.Avg; }
         }
 
         public string pr1
         {
-            get { return ""+m_model.User1+"%"; }
+            get { return "" + (int)m_model.User1 + "%"; }
         }
 
         public string pr2
         {
-            get { return "" + m_model.User2 + "%"; }
+            get { return "" + (int)m_model.User2 + "%"; }
         }
 
         public string pr3
         {
-            get { return "" + m_model.User3 + "%"; }
+            get { return "" + (int)m_model.User3 + "%"; }
         }
 
         public string pr4
         {
-            get { return "" + m_model.User4 + "%"; }
+            get { return "" + (int)m_model.User4 + "%"; }
         }
 
         public string pr5
         {
-            get { return "" + m_model.User5 + "%"; }
+            get { return "" + (int)m_model.User5 + "%"; }
         }
 
         public string pr6
         {
-            get { return "" + m_model.User6 + "%"; }
+            get { return "" + (int)m_model.User6 + "%"; }
         }
 
         public string pr7
         {
-            get { return "" + m_model.User7 + "%"; }
+            get { return "" + (int)m_model.User7 + "%"; }
         }
 
         public string pr8
         {
-            get { return "" + m_model.User8 + "%"; }
+            get { return "" + (int)m_model.User8 + "%"; }
         }
 
         public void start()
